cb69dc3bb38c36b8db2d67a5f833942793f5ad81
Andrew Mukiza <andrewmukiza@gmail.com>
2018-03-27 23:42:34 -0400

    WIP - adds insert for redblack trees.

--- src/RedBlackTree.hs
+++ src/RedBlackTree.hs
@@ -2,11 +2,22 @@ module RedBlackTree where
 
 data Color = R | B deriving (Show, Eq, Ord)
 
-data Tree a = Leaf | Node Color (Tree a) a (Tree a) deriving (Show, Eq, Ord)
+data Tree a = Leaf 
+            | Branch Color (Tree a) a (Tree a) deriving (Show, Eq, Ord)
 
 member :: (Ord a) => a -> Tree a -> Bool
 member _ Leaf = False
-member a (Node _ r v l)
+member a (Branch  _ right v left)
     | a == v = True
-    | a < v = member a r
-    | a > v = member a l
+    | a < v = member a left
+    | a > v = member a right
+
+insert :: (Ord a) => a -> Tree a -> Tree a
+insert a Leaf = Branch B Leaf a Leaf
+insert a node@(Branch color right v left)
+    | a == v = node
+    | a < v = balance (insert a left) right
+    | a > v = balance (insert a right) left
+
+balance :: (Ord a) => Tree a -> Tree a -> Tree a
+balance = undefined
